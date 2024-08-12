8fc53a0ab172a00943a5c8858b63aff54b1ba7ec
Ujhafsuy <duardoescubemaya@gmail.com>
2024-08-11 21:39:35 -0300

    tem coisa pra fzr ainda

--- src/pages/VideoPage/video.js
+++ src/pages/VideoPage/video.js
@@ -1,15 +1,19 @@
 //Função pra expandir os temas
-var temaExpandir = document.getElementsByClassName("tema");
-var contadorDeTemas;
+let temaExpandir = document.getElementsByClassName("tema");
+let contadorDeTemas;
 
-for (contadorDeTemas = 0; contadorDeTemas < temaExpandir.length; contadorDeTemas++) {
-    temaExpandir[contadorDeTemas].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var menuExpansivel = this.nextElementSibling;
-        if (menuExpansivel.style.display === "block") {
-            menuExpansivel.style.display = "none";
-        } else {
-            menuExpansivel.style.display = "block";
-        }
-    });
+for (
+  contadorDeTemas = 0;
+  contadorDeTemas < temaExpandir.length;
+  contadorDeTemas++
+) {
+  temaExpandir[contadorDeTemas].addEventListener("click", function () {
+    this.classList.toggle("active");
+    const menuExpansivel = this.nextElementSibling;
+    if (menuExpansivel.style.display === "block") {
+      menuExpansivel.style.display = "none";
+    } else {
+      menuExpansivel.style.display = "block";
+    }
+  });
 }
