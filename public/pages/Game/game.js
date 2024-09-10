db3e57642f6e47b53eb9ba034bcfbdabafdeb032
David-fj <soprasvr@gmail.com>
2024-09-10 00:35:47 -0300

    asdasd

--- public/pages/Game/game.js
+++ public/pages/Game/game.js
@@ -1,46 +1,76 @@
 const dino = document.querySelector(".dino");
+const obstaculo = document.querySelector(".obstaculo");
 
 let sprite = 0;
 let jump = false;
 let walk = true;
 let margin = 0;
-let coeficiente = 0;
+let fps = 60;
+let xd = 0;
+let start = false;
 
-dino.style.backgroundImage = "url('../../images/sprite/02_sprite.png')";
+dino.style.backgroundImage = "url('../../images/sprite/00_sprite.png')";
 
 
 function imprimir() {
+    function verificarPosicao() {
+        const dinoRet = dino.getBoundingClientRect();
+        const obstaculoRet = obstaculo.getBoundingClientRect();
+
+        if(Math.abs(dinoRet.top - obstaculoRet.top) < 32 && Math.abs(dinoRet.right - obstaculoRet.left) < 32){
+            return true;
+        }else {
+            return false;
+        }
+    }
     document.addEventListener('keydown', (event) => {
         if(event.key === " " && !jump){
             walk = false;
             jump = true;
             dino.style.backgroundImage = "url('../../images/sprite/00_sprite.png')"
-            coeficiente = 5;
+            margin = 16;
+            dino.style.transition = "0.5s";
+            dino.style.margin = `0 0 ${margin}rem 1.5rem`;
         }
+        start = true;
     })
-    if(jump){
-        margin += coeficiente;
-        if(coeficiente > 0)
-            coeficiente--;
-        else
-            coeficiente++;
-        console.log(margin, coeficiente)
-        if(margin == 12){
-            coeficiente = -5;
+    if(start){
+        obstaculo.style.transition = "2s linear";
+        obstaculo.style.display = "flex";
+        void obstaculo.offsetWidth;
+        obstaculo.style.marginLeft = "-6rem";
+
+        obstaculo.addEventListener('transitionend', () => {
+            obstaculo.style.transition = "0s";
+            obstaculo.style.display = "none";
+            obstaculo.style.marginLeft = "88%";
+            obstaculo.style.backgroundImage = `url("../../images/sprite (1)/0${Math.floor(Math.random() * 3)}_sprite (1).png")`
+        }) 
+        if(jump){
+            xd++;
+            if(xd == 5){
+                walk = true;
+                jump = false;
+                dino.style.transition = "0s";
+                xd = 0;
+            }
+            if(xd == 3){
+                margin = 0;
+                dino.style.margin = `0 0 ${margin}rem 1.5rem`;
+            }
         }
-        if(margin == 0){
-            walk = true;
-            jump = false;
+        if(walk){
+            if(sprite == 4){
+                sprite = 2;
+            }
+            dino.style.backgroundImage = `url('../../images/sprite/0${sprite}_sprite.png')`;
+            sprite++;
         }
-        dino.style.margin = `0 0 ${margin}rem 1.5rem`;
-    }
-    if(walk){
-        if(sprite == 4){
-            sprite = 2;
+        if(verificarPosicao()){
+            console.log("Encostou");
+            start = false;
         }
-        dino.style.backgroundImage = `url('../../images/sprite/0${sprite}_sprite.png')`;
-        sprite++;
     }
 }
 
-setInterval(imprimir, 10000/60);
+setInterval(imprimir, 10000/fps);
